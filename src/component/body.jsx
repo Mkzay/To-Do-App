cdc702a33e0ecac515c855fab20a90874341506d
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-10-30 12:00:42 +0100

    function

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -1,4 +1,110 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export default function Body() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() === "") return;
+    setTodos([...todos, { text: newTodo, completed: false }]);
+    setNewTodo("");
+  };
+
+  const toggleTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  const clearCompleted = () => {
+    const updatedTodos = todos.filter((todo) => !todo.completed);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="flex items-center justify-center flex-col gap-5 w-full -mt-28">
+      <div className="flex items-center gap-3 w-11/12 h-14 rounded-md py-3 px-5 bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue md:w-7/12">
+        <input className="" type="checkbox" />
+        <input
+          className="text-lightMode-VeryGrayishBlue dark:text-darkMode-LightGrayishBlue placeholder:text-base placeholder:text-lightMode-DarkGrayishBlue placeholder:dark:text-darkMode-VeryDarkGrayishBlue bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue outline-none"
+          type="text"
+          placeholder="Create a new todo..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
+        />
+      </div>
+      {todos.map((todo, index) => (
+        <div
+          key={index}
+          className={`flex items-center justify-between w-11/12 h-14 rounded-md py-3 px-5 border-b ${
+            todo.completed
+              ? "bg-lightMode-LightGray"
+              : "bg-lightMode-VeryLightGray"
+          } dark:bg-darkMode-VeryDarkBlue md:w-7/12`}
+        >
+          <div className="flex items-center gap-3">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(index)}
+            />
+            <p
+              className={`text-base ${
+                todo.completed ? "line-through" : ""
+              } text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue`}
+            >
+              {todo.text}
+            </p>
+          </div>
+          <div>
+            <button
+              className="dark:text-lightMode-DarkGrayishBlue"
+              onClick={() => deleteTodo(index)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
+          </div>
+        </div>
+      ))}
+      <div className="flex items-center justify-between gap-3 w-11/12 h-14 rounded-md py-3 px-5 bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue md:w-7/12">
+        <p className="text-base text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue">
+          {todos.length} item{todos.length !== 1 ? "s" : ""} left
+        </p>
+        <p
+          className="text-base text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue"
+          onClick={clearCompleted}
+        >
+          Clear Completed
+        </p>
+      </div>
+      <div className="flex items-center justify-center gap-8 w-11/12 h-14 text-base rounded-md py-3 px-5 shadow-xl border-b bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue md:w-7/12 md:shadow-none md:-mt-5">
+        <p className="font-bold text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue">
+          All
+        </p>
+        <p className="font-bold text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue">
+          Active
+        </p>
+        <p className="font-bold text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue">
+          Completed
+        </p>
+      </div>
+    </div>
+  );
+}
+
+/*import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function Body() {
@@ -35,7 +141,7 @@ export default function Body() {
           </p>
         </div>
       </div>
-      <div className="flex items-center justify-center gap-3 w-11/12 h-14 text-base rounded-md py-3 px-5 shadow-xl border-b bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue md:w-7/12 md:shadow-none md:-mt-5">
+      <div className="flex items-center justify-center gap-8 w-11/12 h-14 text-base rounded-md py-3 px-5 shadow-xl border-b bg-lightMode-VeryLightGray dark:bg-darkMode-VeryDarkBlue md:w-7/12 md:shadow-none md:-mt-5">
         <p className="font-bold text-lightMode-DarkGrayishBlue dark:text-darkMode-DarkGrayishBlue">
           All
         </p>
@@ -48,4 +154,4 @@ export default function Body() {
       </div>
     </div>
   );
-}
+}*/
